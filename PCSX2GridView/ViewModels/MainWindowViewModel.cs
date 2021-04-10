2dddbe9aed6ec6ce852f7b0fdfaf8e584fd3cf34
Nikolai Dante <git@nathwilson.co.uk>
2021-04-10 13:45:01 +0100

    refactor: alter view model to facilitate unit testing

--- PCSX2GridView/ViewModels/MainWindowViewModel.cs
+++ PCSX2GridView/ViewModels/MainWindowViewModel.cs
@@ -11,29 +11,25 @@
 
     public class MainWindowViewModel : ViewModelBase
     {
+        private IApplicationAssetService assetService = null!;
+
         public MainWindowViewModel()
         {
+            this.Initialize(new GameMediaService(new PhysicalFileProvider(this.RootFilePath)), new CoverArtService(new PhysicalFileProvider(this.RootCoverPath)));
+            RxApp.MainThreadScheduler.Schedule(this.LoadLibrary);
+        }
+
+        public MainWindowViewModel(IGameMediaService gameMediaService, ICoverArtService artService)
+        {
+            this.Initialize(gameMediaService, artService);
             RxApp.MainThreadScheduler.Schedule(this.LoadLibrary);
         }
 
         public ObservableCollection<LibraryItemViewModel> Games { get; } = new ();
 
-        private static async Task<IEnumerable<LibraryItemViewModel>> LoadCached()
-        {
-            var result = new List<LibraryItemViewModel>();
+        private string RootFilePath => "/home/chane/Games/ROMs/PlayStation2";
 
-            var rootFilePath = "/home/chane/Games/ROMs/PlayStation2";
-            var rootCoverPath = $"{rootFilePath}/Covers";
-
-            var gameProvider = new PhysicalFileProvider(rootFilePath);
-            var gameMediaService = new GameMediaService(gameProvider);
-
-            var artProvider = new PhysicalFileProvider(rootCoverPath);
-            var artService = new CoverArtService(artProvider);
-
-            var assetService = new ApplicationAssetService(gameMediaService, artService);
-
-            var games = await assetService.Fetch().ConfigureAwait(false);
+        private string RootCoverPath => $"{this.RootFilePath}/Covers";
 
             foreach (var game in games)
             {
@@ -47,12 +43,14 @@
                 result.Add(item);
             }
 
-            return result.OrderBy(g => g.GameName);
+        private void Initialize(IGameMediaService gameMediaService, ICoverArtService artService)
+        {
+            this.assetService = new ApplicationAssetService(gameMediaService, artService);
         }
 
         private async void LoadLibrary()
         {
-            var items = await LoadCached().ConfigureAwait(false);
+            var items = await this.LoadGames().ConfigureAwait(false);
 
             foreach (var item in items)
             {
@@ -60,6 +58,27 @@
             }
 
             this.LoadCovers();
+        }
+
+        private async Task<IEnumerable<LibraryItemViewModel>> LoadGames()
+        {
+            var result = new List<LibraryItemViewModel>();
+
+            var games = await this.assetService.Fetch().ConfigureAwait(false);
+
+            foreach (var game in games)
+            {
+                var item = new LibraryItemViewModel
+                {
+                    GameName = game.FileName,
+                    PhysicalPath = $"{this.RootFilePath}/{game.FileName}",
+                    CoverArt = string.IsNullOrEmpty(game.CoverArt) ? string.Empty : $"{this.RootCoverPath}/{game.CoverArt}",
+                };
+
+                result.Add(item);
+            }
+
+            return result.OrderBy(g => g.GameName);
         }
 
         private async void LoadCovers()
